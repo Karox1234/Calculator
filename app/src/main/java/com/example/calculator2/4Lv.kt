0f561907bbd22372750d6db1603e8cb0f8eda8c4
Karox1234 <zzzcomet@gmail.com>
2023-12-07 17:20:59 +0900

    3lv 4lv 제출용

---
+++ app/src/main/java/com/example/calculator2/4Lv.kt
@@ -0,0 +1,63 @@
+package com.example.calculator2
+
+fun main() {
+    println("첫번째 수를 입력하세요")
+    val num1 = readLine()!!.toDouble()
+    println("원하시는 사칙연산 기호를 입력하세요")
+    val oper = readLine()!!.toString()
+    println("두번째 수를 입력하세요")
+    val num2 = readLine()!!.toDouble()
+    val calc = Calculator()
+    var rel = calc.cac(num1,oper,num2)
+
+
+    println("계산결과 ${rel} 입니다")
+}
+class Calculator{
+    fun cac(num1: Double,oper:String,num2: Double): Double {
+        var result: AbstractOperation = when (oper) {
+            "+" -> (AddOperation())
+            "-" -> (SubstractOperation())
+            "*" -> (MultiplyOperation())
+            "/" -> (DivideOperation())
+            else -> (ellse())
+        }
+        return result.operate(num1, num2)
+    }
+}
+
+open abstract class AbstractOperation{
+    open abstract fun operate(num1: Double, num2: Double): Double
+
+}
+class AddOperation:AbstractOperation(){
+   override fun operate(num1: Double, num2: Double): Double {
+        return (num1+num2)
+    }
+}
+
+class SubstractOperation:AbstractOperation() {
+    override fun operate(num1: Double, num2: Double): Double {
+         return (num1 - num2)
+    }
+}
+
+class MultiplyOperation:AbstractOperation() {
+   override fun operate(num1: Double, num2: Double): Double {
+         return (num1 * num2)
+
+    }
+}
+
+class DivideOperation:AbstractOperation() {
+   override fun operate(num1: Double, num2: Double): Double {
+        return  (num1 / num2)
+
+    }
+}
+
+class ellse:AbstractOperation() {
+    override fun operate(num1: Double, num2: Double): Double {
+        return 0.0
+    }
+}
