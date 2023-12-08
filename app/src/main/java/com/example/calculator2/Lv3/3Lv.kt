d20dad11085afba419eda17fa6806514cca4992d
Karox1234 <zzzcomet@gmail.com>
2023-12-08 11:37:29 +0900

    폴더분리 및 4Lv 약간 수정

---
+++ app/src/main/java/com/example/calculator2/Lv3/3Lv.kt
@@ -0,0 +1,60 @@
+package com.example.calculator2.Lv3
+
+fun main() {
+    println("첫번째 수를 입력하세요")
+    val num1 = readLine()!!.toDouble()
+    println("원하시는 사칙연산 기호를 입력하세요")
+    var oper = readLine()!!.toString()
+    //조건문 넣어보기
+    println("두번째 수를 입력하세요")
+    val num2 = readLine()!!.toDouble()
+    val calc = Calculator()
+    val rel = calc.operate(num1, oper, num2)
+
+    println("계산결과 ${rel} 입니다")
+
+}
+
+class Calculator {
+    fun operate(num1: Double, oper: String, num2: Double): Double {
+        var result = when (oper) {
+            "+" -> (AddOperation().operate(num1, num2))
+            "-" -> (SubstractOperation().operate(num1, num2))
+            "*" -> (MultiplyOperation().operate(num1, num2))
+            "/" -> (DivideOperation().operate(num1, num2))
+            "%" -> (num1 % num2)
+            else -> {
+                0.0
+            }
+        }
+        return result
+    }
+}
+
+class AddOperation {
+    fun operate(num1: Double, num2: Double): Double {
+        var result = (num1 + num2)
+        return result
+    }
+}
+
+class SubstractOperation {
+    fun operate(num1: Double, num2: Double): Double {
+        var result = (num1 - num2)
+        return result
+    }
+}
+
+class MultiplyOperation {
+    fun operate(num1: Double, num2: Double): Double {
+        var result = (num1 * num2)
+        return result
+    }
+}
+
+class DivideOperation {
+    fun operate(num1: Double, num2: Double): Double {
+        var result = (num1 / num2)
+        return result
+    }
+}
---
+++ app/src/main/java/com/example/calculator2/Lv4/4Lv.kt
@@ -0,0 +1,79 @@
+package com.example.calculator2.Lv4
+
+import TestFolder.Calculator
+
+fun main() {
+    println("첫번째 수를 입력하세요")
+    val num1 = readLine()!!.toDouble()
+    val oper:String
+    while (true) {
+        println("원하시는 사칙연산 기호를 입력하세요")
+        val opertest = readLine()!!.toString()
+        when (opertest) {
+            "+", "-","*","/" -> {
+                oper = opertest
+                break
+            }
+            else -> {
+                println("잘못된 기호입니다")
+                continue
+            }
+        }
+
+    }
+    println("두번째 수를 입력하세요")
+    val num2 = readLine()!!.toDouble()
+    val calc = Calculator()
+    var rel = calc.cac(num1, oper, num2)
+
+
+    println("계산결과 ${rel} 입니다")
+}
+class Calculator{
+    fun cac(num1: Double,oper:String,num2: Double): Double {
+        var result: AbstractOperation = when (oper) {
+            "+" -> (AddOperation())
+            "-" -> (SubstractOperation())
+            "*" -> (MultiplyOperation())
+            "/" -> (DivideOperation())
+            else -> (ellse())
+        }
+        return result.operate(num1, num2)
+    }
+}
+
+open abstract class AbstractOperation{
+    open abstract fun operate(num1: Double, num2: Double): Double
+
+}
+class AddOperation:AbstractOperation(){
+   override fun operate(num1: Double, num2: Double): Double {
+        return (num1+num2)
+    }
+}
+
+class SubstractOperation:AbstractOperation() {
+    override fun operate(num1: Double, num2: Double): Double {
+         return (num1 - num2)
+    }
+}
+
+class MultiplyOperation:AbstractOperation() {
+   override fun operate(num1: Double, num2: Double): Double {
+         return (num1 * num2)
+
+    }
+}
+
+class DivideOperation:AbstractOperation() {
+   override fun operate(num1: Double, num2: Double): Double {
+        return  (num1 / num2)
+
+    }
+}
+
+class ellse:AbstractOperation() {
+    override fun operate(num1: Double, num2: Double): Double {
+        return 0.0
+    }
+}
